567ddac4745e1d7e66de1187294d721772571b2a
h-julianna <harangozo.jazmin@gmail.com>
2025-11-23 14:07:04 +0100

    mostly working randomization + trials.json

--- randomization.py
+++ randomization.py
@@ -2,10 +2,10 @@ import random
 import json
 import pandas as pd
 
-
-# global variables to be manipulated
-stimulus_set = []
-relay = 0
+# global
+TRIALS_PER_SET = 100
+TOTAL_SETS = 10
+colors = ["black", "red", "green", "yellow", "blue"]
 
 #defining stimulus set
 #black stimuli
@@ -16,7 +16,6 @@ black_vertical_con = [
 black_vertical_inc = [
     {"prime": "fel\nfel\nfel", "probe": "le", "congruency": "incongruent", "correct_response": "n", "name": "black_vertical_incon_1"},
     {"prime": "le\nle\nle", "probe": "fel", "congruency": "incongruent", "correct_response": "j", "name": "black_vertical_incon_2"}
-
 ]
 
 black_horizontal_con = [
@@ -24,88 +23,83 @@ black_horizontal_con = [
     {"prime": "jobb\njobb\njobb", "probe": "jobb", "congruency": "congruent", "correct_response": "g", "name": "black_horizontal_con_2"}
 ]
 black_horizontal_incon = [
-    {"prime": "bal\nbal\nbal", "probe": "jobb", "congruency": "incongruent", "correct_response": "g",
-     "name": "black_horizontal_incon_1"},
-    {"prime": "jobb\njobb\njobb", "probe": "bal", "congruency": "incongruent", "correct_response": "f",
-     "name": "black_horizontal_incon_2"}
+    {"prime": "bal\nbal\nbal", "probe": "jobb", "congruency": "incongruent", "correct_response": "g", "name": "black_horizontal_incon_1"},
+    {"prime": "jobb\njobb\njobb", "probe": "bal", "congruency": "incongruent", "correct_response": "f", "name": "black_horizontal_incon_2"}
 ]
-
+#red stimuli
 red_vertical_con = [
-    {"prime":"le", "probe":"le", "congruency":"congruent", "correct_response":"n", "name":"red_vertical_con_1", "color":"red"},
-    {"prime":"fel", "probe":"fel", "congruency":"congruent", "correct_response":"e", "name":"red_vertical_con_2", "color":"red"}
+    {"prime": "le\nle\nle", "probe":"le", "congruency":"congruent", "correct_response":"n", "name":"red_vertical_con_1", "color":"red"},
+    {"prime":"fel\nfel\nfel", "probe":"fel", "congruency":"congruent", "correct_response":"e", "name":"red_vertical_con_2", "color":"red"}
 ]
 red_vertical_incon = [
-    {"prime":"fel", "probe":"le", "congruency":"incongruent", "correct_response":"n", "name":"red_vertical_incon_1", "color":"red"},
-    {"prime":"le", "probe":"fel", "congruency":"incongruent", "correct_response":"e", "name":"red_vertical_incon_2", "color":"red"}
+    {"prime":"fel\nfel\nfel", "probe":"le", "congruency":"incongruent", "correct_response":"n", "name":"red_vertical_incon_1", "color":"red"},
+    {"prime":"le\nle\nle", "probe":"fel", "congruency":"incongruent", "correct_response":"e", "name":"red_vertical_incon_2", "color":"red"}
 ]
 
 red_horizontal_con = [
-    {"prime":"bal", "probe":"bal", "congruency":"congruent", "correct_response":"l", "name":"red_horizontal_con_1", "color":"red"},
-    {"prime":"jobb", "probe":"jobb", "congruency":"congruent", "correct_response":"a", "name":"red_horizontal_con_2", "color":"red"}
+    {"prime":"bal\nbal\nbal", "probe":"bal", "congruency":"congruent", "correct_response":"l", "name":"red_horizontal_con_1", "color":"red"},
+    {"prime":"jobb\njobb\njobb", "probe":"jobb", "congruency":"congruent", "correct_response":"a", "name":"red_horizontal_con_2", "color":"red"}
 ]
 red_horizontal_incon = [
-    {"prime":"bal", "probe":"jobb", "congruency":"incongruent", "correct_response":"l", "name":"red_horizontal_incon_1", "color":"red"},
-    {"prime":"jobb", "probe":"bal", "congruency":"incongruent", "correct_response":"a", "name":"red_horizontal_incon_2", "color":"red"}
+    {"prime":"bal\nbal\nbal", "probe":"jobb", "congruency":"incongruent", "correct_response":"l", "name":"red_horizontal_incon_1", "color":"red"},
+    {"prime":"jobb\njobb\njobb", "probe":"bal", "congruency":"incongruent", "correct_response":"a", "name":"red_horizontal_incon_2", "color":"red"}
 ]
-
 #green stimuli
 green_vertical_con = [
-    {"prime":"le", "probe":"le", "congruency":"congruent", "correct_response":"n", "name":"green_vertical_con_1", "color":"green"},
-    {"prime":"fel", "probe":"fel", "congruency":"congruent", "correct_response":"e", "name":"green_vertical_con_2", "color":"green"}
+    {"prime":"le\nle\nle", "probe":"le", "congruency":"congruent", "correct_response":"n", "name":"green_vertical_con_1", "color":"green"},
+    {"prime":"fel\nfel\nfel", "probe":"fel", "congruency":"congruent", "correct_response":"e", "name":"green_vertical_con_2", "color":"green"}
 ]
 green_vertical_incon = [
-    {"prime":"fel", "probe":"le", "congruency":"incongruent", "correct_response":"n", "name":"green_vertical_incon_1", "color":"green"},
-    {"prime":"le", "probe":"fel", "congruency":"incongruent", "correct_response":"e", "name":"green_vertical_incon_2", "color":"green"}
+    {"prime":"fel\nfel\nfel", "probe":"le", "congruency":"incongruent", "correct_response":"n", "name":"green_vertical_incon_1", "color":"green"},
+    {"prime":"le\nle\nle", "probe":"fel", "congruency":"incongruent", "correct_response":"e", "name":"green_vertical_incon_2", "color":"green"}
 ]
 
 green_horizontal_con = [
-    {"prime":"bal", "probe":"bal", "congruency":"congruent", "correct_response":"l", "name":"green_horizontal_con_1", "color":"green"},
-    {"prime":"jobb", "probe":"jobb", "congruency":"congruent", "correct_response":"a", "name":"green_horizontal_con_2", "color":"green"}
+    {"prime":"bal\nbal\nbal", "probe":"bal", "congruency":"congruent", "correct_response":"l", "name":"green_horizontal_con_1", "color":"green"},
+    {"prime":"jobb\njobb\njobb", "probe":"jobb", "congruency":"congruent", "correct_response":"a", "name":"green_horizontal_con_2", "color":"green"}
 ]
 green_horizontal_incon = [
-    {"prime":"bal", "probe":"jobb", "congruency":"incongruent", "correct_response":"l", "name":"green_horizontal_incon_1", "color":"green"},
-    {"prime":"jobb", "probe":"bal", "congruency":"incongruent", "correct_response":"a", "name":"green_horizontal_incon_2", "color":"green"}
+    {"prime":"bal\nbal\nbal", "probe":"jobb", "congruency":"incongruent", "correct_response":"l", "name":"green_horizontal_incon_1", "color":"green"},
+    {"prime":"jobb\njobb\njobb", "probe":"bal", "congruency":"incongruent", "correct_response":"a", "name":"green_horizontal_incon_2", "color":"green"}
 ]
-
-#yellow stimuli idk
+#yellow stimuli
 yellow_vertical_con = [
-    {"prime":"le", "probe":"le", "congruency":"congruent", "correct_response":"n", "name":"yellow_vertical_con_1", "color":"yellow"},
-    {"prime":"fel", "probe":"fel", "congruency":"congruent", "correct_response":"e", "name":"yellow_vertical_con_2", "color":"yellow"}
+    {"prime":"le\nle\nle", "probe":"le", "congruency":"congruent", "correct_response":"n", "name":"yellow_vertical_con_1", "color":"yellow"},
+    {"prime":"fel\nfel\nfel", "probe":"fel", "congruency":"congruent", "correct_response":"e", "name":"yellow_vertical_con_2", "color":"yellow"}
 ]
 yellow_vertical_incon = [
-    {"prime":"fel", "probe":"le", "congruency":"incongruent", "correct_response":"n", "name":"yellow_vertical_incon_1", "color":"yellow"},
-    {"prime":"le", "probe":"fel", "congruency":"incongruent", "correct_response":"e", "name":"yellow_vertical_incon_2", "color":"yellow"}
+    {"prime":"fel\nfel\nfel", "probe":"le", "congruency":"incongruent", "correct_response":"n", "name":"yellow_vertical_incon_1", "color":"yellow"},
+    {"prime":"le\nle\nle", "probe":"fel", "congruency":"incongruent", "correct_response":"e", "name":"yellow_vertical_incon_2", "color":"yellow"}
 ]
 
 yellow_horizontal_con = [
-    {"prime":"bal", "probe":"bal", "congruency":"congruent", "correct_response":"l", "name":"yellow_horizontal_con_1", "color":"yellow"},
-    {"prime":"jobb", "probe":"jobb", "congruency":"congruent", "correct_response":"a", "name":"yellow_horizontal_con_2", "color":"yellow"}
+    {"prime":"bal\nbal\nbal", "probe":"bal", "congruency":"congruent", "correct_response":"l", "name":"yellow_horizontal_con_1", "color":"yellow"},
+    {"prime":"jobb\njobb\njobb", "probe":"jobb", "congruency":"congruent", "correct_response":"a", "name":"yellow_horizontal_con_2", "color":"yellow"}
 ]
 yellow_horizontal_incon = [
-    {"prime":"bal", "probe":"jobb", "congruency":"incongruent", "correct_response":"l", "name":"yellow_horizontal_incon_1", "color":"yellow"},
-    {"prime":"jobb", "probe":"bal", "congruency":"incongruent", "correct_response":"a", "name":"yellow_horizontal_incon_2", "color":"yellow"}
+    {"prime":"bal\nbal\nbal", "probe":"jobb", "congruency":"incongruent", "correct_response":"l", "name":"yellow_horizontal_incon_1", "color":"yellow"},
+    {"prime":"jobb\njobb\njobb", "probe":"bal", "congruency":"incongruent", "correct_response":"a", "name":"yellow_horizontal_incon_2", "color":"yellow"}
 ]
 
-#blue stimuli idk
+#blue stimuli
 blue_vertical_con = [
-    {"prime":"le", "probe":"le", "congruency":"congruent", "correct_response":"n", "name":"blue_vertical_con_1", "color":"blue"},
-    {"prime":"fel", "probe":"fel", "congruency":"congruent", "correct_response":"e", "name":"blue_vertical_con_2", "color":"blue"}
+    {"prime":"le\nle\nle", "probe":"le", "congruency":"congruent", "correct_response":"n", "name":"blue_vertical_con_1", "color":"blue"},
+    {"prime":"fel\nfel\nfel", "probe":"fel", "congruency":"congruent", "correct_response":"e", "name":"blue_vertical_con_2", "color":"blue"}
 ]
 blue_vertical_incon = [
-    {"prime":"fel", "probe":"le", "congruency":"incongruent", "correct_response":"n", "name":"blue_vertical_incon_1", "color":"blue"},
-    {"prime":"le", "probe":"fel", "congruency":"incongruent", "correct_response":"e", "name":"blue_vertical_incon_2", "color":"blue"}
+    {"prime":"fel\nfel\nfel", "probe":"le", "congruency":"incongruent", "correct_response":"n", "name":"blue_vertical_incon_1", "color":"blue"},
+    {"prime":"le\nle\nle", "probe":"fel", "congruency":"incongruent", "correct_response":"e", "name":"blue_vertical_incon_2", "color":"blue"}
 ]
 
 blue_horizontal_con = [
-    {"prime":"bal", "probe":"bal", "congruency":"congruent", "correct_response":"l", "name":"blue_horizontal_con_1", "color":"blue"},
-    {"prime":"jobb", "probe":"jobb", "congruency":"congruent", "correct_response":"a", "name":"blue_horizontal_con_2", "color":"blue"}
+    {"prime":"bal\nbal\nbal", "probe":"bal", "congruency":"congruent", "correct_response":"l", "name":"blue_horizontal_con_1", "color":"blue"},
+    {"prime":"jobb\njobb\njobb", "probe":"jobb", "congruency":"congruent", "correct_response":"a", "name":"blue_horizontal_con_2", "color":"blue"}
 ]
 blue_horizontal_incon = [
-    {"prime":"bal", "probe":"jobb", "congruency":"incongruent", "correct_response":"l", "name":"blue_horizontal_incon_1", "color":"blue"},
-    {"prime":"jobb", "probe":"bal", "congruency":"incongruent", "correct_response":"a", "name":"blue_horizontal_incon_2", "color":"blue"}
+    {"prime":"bal\nbal\nbal", "probe":"jobb", "congruency":"incongruent", "correct_response":"l", "name":"blue_horizontal_incon_1", "color":"blue"},
+    {"prime":"jobb\njobb\njobb", "probe":"bal", "congruency":"incongruent", "correct_response":"a", "name":"blue_horizontal_incon_2", "color":"blue"}
 ]
 
-# grouping congruent and incongruent stimuli together
 vertical_group = [
     black_vertical_con,
     black_vertical_inc,
@@ -130,112 +124,65 @@ horizontal_group = [
     blue_horizontal_con,
     blue_horizontal_incon]
 
-def stimulus_finder(group, allow_red=True):
-    global stimulus_set
+#for 50-50 congruency
+def half_split(n):
+    return n // 2, n - n // 2
 
-    # red trials are 30% of all trials
-    is_red = allow_red and (random.random() < 0.3)
+def generate_trial_block(trials_per_block=100):
+    block = []
+    relay = 0  # for orientation alternation
 
-    if is_red:
-        # choosing from red lists
-        chosen_list = random.choice(group[2:4])
-    else:
-        # choosing from neutral lists
-        chosen_list = random.choice(group[0:2])
-
-    stimulus = random.choice(chosen_list)
-    stimulus_set.append(stimulus)
-
-
-def set_factory(times):
-    global stimulus_set
-    global relay
-
-    for i in range(times):
-        # first trial must be neutral
-        allow_red = i != 0
+    for i in range(trials_per_block):
         if relay == 0:
-            stimulus_finder(horizontal_group, allow_red)
+            group = horizontal_group
             relay = 1
         else:
-            stimulus_finder(vertical_group, allow_red)
+            group = vertical_group
             relay = 0
 
+        #picking colour
+        color_idx = random.choice(range(0, len(colors)))
+        color_slice = color_idx * 2
+        congruency_idx = color_slice + random.choice([0, 1])
+        trial_list = group[congruency_idx]
+        trial = random.choice(trial_list).copy()
+        trial["color"] = colors[color_idx]
 
-def group_relay():
-    global relay
-    if relay == 0:
-        stimulus_finder(horizontal_group)
-        relay = 1
-    else:
-        stimulus_finder(vertical_group)
-        relay = 0
+        block.append(trial)
+
+    #+1 first trial
+    first_orientation = "vertical" if "vertical" in block[0]["name"] else "horizontal"
+    opposite_group = vertical_group if first_orientation == "horizontal" else horizontal_group
+    allowed_colors = ["black", "yellow", "blue"]
+    color_idx = random.choice([colors.index(c) for c in allowed_colors])
+    color_slice = color_idx * 2
+    congruency_idx = color_slice + random.choice([0, 1])
+    trial_list = opposite_group[congruency_idx]
+    first_trial = random.choice(trial_list).copy()
+    first_trial["color"] = colors[color_idx]
+
+    block.insert(0, first_trial)
+
+    return block
+
+def save_trials_to_json(all_sets, file_path="trials.json"):
+    try:
+        with open(file_path, "r") as f:
+            file_data = json.load(f)
+        if "trials" not in file_data:
+            file_data = {"trials": []}
+    except (FileNotFoundError, json.JSONDecodeError):
+        file_data = {"trials": []}
+    file_data["trials"].extend(all_sets)
+    with open(file_path, "w") as f:
+        json.dump(file_data, f, indent=2)
 
 
-def set_factory(times):
-    for i in range(times):
-        group_relay()
+all_sets = []
+for i in range(TOTAL_SETS):
+    block = generate_trial_block()
+    all_sets.append(block)
+    print(f"Set {i+1}:")
+save_trials_to_json(all_sets)
 
-
-trial_rate = []
-trial_count = 90
-
-
-def factor():
-    global trial_rate
-    global trial_count
-    set_factory(trial_count)
-    con_count = 0
-    incon_count = 0
-    for i in stimulus_set:
-        if i["congruency"] == "congruent":
-            con_count += 1
-        elif i["congruency"] == "incongruent":
-            incon_count += 1
-    con = con_count / trial_count
-    incon = incon_count / trial_count
-    trial_rate = [con, incon]
-    
-    factor()
-
-
-# double check for red balance ~ serious vibe coding shit
-def check_red_balance():
-    df = pd.DataFrame(stimulus_set)
-    red_trials = df[df.get("color") == "red"]
-
-    counts = red_trials.groupby(["congruency"]).size()
-    print("Red congruency counts:\n", counts)
-
-    hv_counts = []
-    for s in stimulus_set:
-        if "color" in s and s["color"] == "red":
-            hv_counts.append("horizontal" if "horizontal" in s["name"] else "vertical")
-    hv_df = pd.Series(hv_counts).value_counts()
-    print("Red orientation counts:\n", hv_df)
-
-match_count = 0
-while True:
-    if trial_rate == [0.5, 0.5]:
-        group_relay()
-        trial_block = pd.DataFrame(stimulus_set, columns=["prime", "probe", "congruency", "correct_response"])
-        trial_block["previous_congruency"] = trial_block["congruency"].shift(1)
-        trial_block["con_pair"] = trial_block["previous_congruency"] + "-" + trial_block["congruency"]
-        pair_counts = trial_block["con_pair"].value_counts()
-        if pair_counts["congruent-congruent"] == 20 and pair_counts["congruent-incongruent"] == 20 and pair_counts[
-            "incongruent-congruent"] == 20 and pair_counts["incongruent-incongruent"] == 20:
-            match_count += 1
-            print("Match found: {}".format(match_count))
-            print("Correct paircount found, writing...")
-            with open("trials.json", "r+") as file:
-                # First we load existing data into a dictionary
-                file_data = json.load(file)
-                # join new_data with file_data inside emp_details
-                file_data["trials"].append(stimulus_set)
-                # Sets file's current position at offset.
-                file.seek(0)
-                # convert back to json.
-                json.dump(file_data, file)
-
-    stimulus_set = []
-    factor()
+print(f"{TOTAL_SETS} trial blocks saved to trials.json")
